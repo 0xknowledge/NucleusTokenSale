8f396d4dcc9bb4801d9b55556bac43f11a89e6a2
Prathab Murugesan <prathabmurugesan@Prathabs-MacBook-Pro.local>
2018-01-24 19:31:36 +0530

    update to use standard token

--- contracts/NucleusCoreToken.sol
+++ contracts/NucleusCoreToken.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.4.18;
 
-import "zeppelin-solidity/contracts/token/MintableToken.sol";
-import "zeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "zeppelin-solidity/contracts/token/StandardToken.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
  * @title NucleusCoreToken
  * @dev NucleusCoreToken (ERC20) contract defining basic parameters of a ERC20 Token
  */
 
-contract NucleusCoreToken is MintableToken, Pausable {
+contract NucleusCoreToken is StandardToken, Ownable {
   string public constant name = 'NucleusCore';
   string public constant symbol = 'nCore';
   uint8 public constant decimals = 0;
@@ -17,6 +17,24 @@ contract NucleusCoreToken is MintableToken, Pausable {
    * @dev totalSupply is not set as we don't know how many investors will get the core token
    */
   function NucleusCoreToken() public {
+  }
+
+  /**
+   * @dev Function to mint tokens
+   * @param recipients The list of addresses eligible to get a NucleusCoreToken
+   */
+  function mint(address[] recipients) onlyOwner {
+    uint newRecipients = 0;
+    for( uint i = 0 ; i < recipients.length ; i++ ){
+      address recipient = recipients[i];
+      if(balances[recipient] == 0 ){
+        Transfer( address(0x0), recipient, 1 );
+        balances[recipient] = 1;
+	newRecipients++;
+      }
+    }
+    
+    totalSupply = totalSupply.add(newRecipients);
   }
 
   // nCore tokens are not transferrable
